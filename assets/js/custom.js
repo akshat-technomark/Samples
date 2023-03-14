b8ac42b76974deb09ebc4f11b7ff03b84d37d1db
PACIFICABS\akshat.mehta <akshat.mehta@technomark.io>
2023-03-14 13:36:30 +0530

    changes

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+function view_form(){
+    var form_div = document.getElementById("form_data");
+    form_div.classList.add("visiblity_form");
+}
+function submitDetails(){
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var oname = document.getElementById('office_name').value;
+}
